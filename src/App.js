db58bfc62f4cc34b233bdab2ab61c21edb2a69ac
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-04-04 03:21:51 +0530

    1 - intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState("123");
+
+  const handleOnChange = (event) => {
+    setNumber(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 m-auto py-5 text-center">
+            <input
+              onChange={handleOnChange}
+              value={number}
+              type="text"
+              className="form-control"
+              placeholder="enter full number"
+            />
+
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href={`http://wa.me/91${number}`}
+              className="btn btn-primary my-3 m-auto"
+            >
+              Chat on whatsapp
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
