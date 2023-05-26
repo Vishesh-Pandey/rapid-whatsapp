bbee13a57adb5a67a1f44aff1294c3c81d85540a
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-05-26 23:37:04 +0530

    fix: chat from history + History UI update

--- src/components/History.js
+++ src/components/History.js
@@ -28,40 +28,47 @@ const History = ({ contactHistory, setContactHistory }) => {
     });
     localStorage.setItem("history", JSON.stringify(modifiedHistory));
     setContactHistory(JSON.parse(localStorage.getItem("history")));
-  }
+  };
 
   return (
-    <div className='col-md-6 py-1'>
-      <div className='d-flex justify-content-between align-items-center '>
-        <span className='fw-border-4 fs-4'>History</span>
-        <button onClick={clearHistory} className='btn btn-outline-dark mx-1 d-flex'>
-          <i className='bi bi-trash3' />
+    <div className="col-md-6 py-1">
+      <div className="d-flex justify-content-between align-items-center ">
+        <span className="fw-border-4 fs-4">History</span>
+        <button
+          onClick={clearHistory}
+          className="btn btn-outline-dark mx-1 d-flex"
+        >
+          <i className="bi bi-trash3" />
         </button>
       </div>
 
       <div>
         {contactHistory.map((element, index) => {
           return (
-            <div className='d-flex'>
-              <a
-                key={index}
-                target='_blank'
-                rel='noreferrer'
-                href={`http://wa.me/${element}`}
-                className='btn btn-outline-dark w-100 my-2'
-              >
-                <div>
-                  <i className='bi bi-whatsapp mx-3' />
-                  {element.number}
-                </div>
-                <time style={{ fontSize: "12px" }}>{element.timedate}</time>
-              </a>
-              <div className="my-4 ml-2">
-                <button onClick={e => clearIndividualChatHistory(element)} value={element} 
-                  className='btn btn-outline-dark m-1'>
-                  <i className='bi bi-trash3' />
+            <div key={index} className="btn btn-outline-dark w-100 my-2">
+              <div>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`http://wa.me/${element.number}`}
+                  className="btn btn-outline-success btn-sm border-0 w-75"
+                >
+                  <i className="bi bi-whatsapp mx-3" />
+                  <span>{element.number}</span>
+                </a>
+
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    clearIndividualChatHistory(element);
+                  }}
+                  value={element}
+                  className="btn btn-outline-danger btn-sm border-0 float-end w-25"
+                >
+                  <i className="bi bi-trash3" />
                 </button>
               </div>
+              <time style={{ fontSize: "12px" }}>{element.timedate}</time>
             </div>
           );
         })}
